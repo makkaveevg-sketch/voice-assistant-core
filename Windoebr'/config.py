241d4887b97806140f11fd1792ab4d6d70682835
Andrey-Shinomontag <futbolist2009@gmail.com>
2026-06-07 20:30:39 +0300

    Ященко ахуел

--- Windoebr'/config.py
+++ Windoebr'/config.py
@@ -1,3 +1,6 @@
 APPS = {
-    "discord": r"C:\Users\Andrey\AppData\Local\Discord\Update.exe --processStart Discord.exe"
+    "discord": r"C:\Users\Andrey\AppData\Local\Discord\Update.exe --processStart Discord.exe",
+    "yandex": r"C:\Program Files\Yandex\YandexBrowser\Application\browser.exe",
+    "vscode": r"D:\Microsoft VS Code\Code.exe",
+    "steam": r"C:\Program Files\Steam\Steam.exe"
 }
